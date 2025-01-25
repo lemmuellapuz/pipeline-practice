9032a3ed93390236bb704c683a69c47f4a74ce95
unknown <lapuz.lemmuel@gmail.com>
2025-01-25 15:14:53 +0800

    sonarqube warning fix

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use App\Http\Controllers\User\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::apiResource('/user', UserController::class);
